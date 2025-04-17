4c6bb9358113b94389c644c8ca8022b2eac40ecf
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-17 08:55:18 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
  
-local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
+local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/Beta.lua", true))()
 
 local Window = Fluent:CreateWindow({
     Title = "Meu Script",
