4e35e8587b6e230a3e93ffb0ef53a76abbf6a380
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-05-02 20:09:26 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -645,31 +645,3 @@ creditos:AddParagraph({
   Content = "O sistema principal (exceto a função de voo) e train também foram feitos por Lucas."
 })
 
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-InterfaceManager:SetLibrary(Fluent)
-
-local conf = Window:AddTab({
-    Title = "configurações", 
-    Icon = "settings"
-})
-
-InterfaceManager:BuildInterfaceSection(conf)
-
-local function deleteKey()
-    if isfile("FluentUI_KeySystem.txt") then
-        delfile("FluentUI_KeySystem.txt")
-    end
-end
-
-conf:AddButton({
-    Title = "Destroy Key",
-    Description = "Remove a key salva localmente",
-    Callback = function()
-        deleteKey()
-        Fluent:Notify({
-            Title = "Key Removida",
-            Content = "A key salva foi removida com sucesso!",
-            Duration = 3
-        })
-    end
-})
